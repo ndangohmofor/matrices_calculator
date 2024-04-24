01e833c723314545f5dda7d9162f2546e03c0860
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-24 06:26:03 -0400

    Add matrix generator class

---
+++ src/main/java/org/example/MatricesGenerator.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class MatricesGenerator {
+    private static final String OUTPUT_FILE = "./out/matrices";
+    private static final int N = 10;
+    private static final int NUMBER_OF_MATRICES_PAIRS = 100000;
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(OUTPUT_FILE);
+        FileWriter fileWriter = new FileWriter(file);
+        createMatrices(fileWriter);
+        fileWriter.flush();
+        fileWriter.close();
+    }
+
+    private static float[] createRow(Random random) {
+        float[] row = new float[N];
+        for (int i = 0; i < N; i++) {
+            row[i] = random.nextFloat() * random.nextInt(100);
+        }
+        return row;
+    }
+}
