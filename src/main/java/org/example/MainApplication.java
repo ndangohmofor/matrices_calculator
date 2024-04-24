a3685695702f33f6ee29d870e1d38a473b2abcc2
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-24 07:28:46 -0400

    create main application with linkedlist based queue and methods to add, remove and terminate the queue

---
+++ src/main/java/org/example/MainApplication.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MainApplication {
+    public static void main(String[] args) {
+    }
+
+    private static class ThreadSafeQueue {
+        private Queue<MatricesPair> queue = new LinkedList<>();
+        private boolean isEmpty = true;
+        private boolean isTerminate = false;
+
+        public synchronized void add(MatricesPair matricesPair) {
+            queue.add(matricesPair);
+            isEmpty = false;
+            notify();
+        }
+
+        public synchronized MatricesPair remove() {
+            while (isEmpty && !isTerminate) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                }
+            }
+            if (queue.size() == 1) {
+                isEmpty = true;
+            }
+            if (queue.size() == 0 && isTerminate) {
+                return null;
+            }
+            System.out.println("queue size: " + queue.size());
+            return queue.remove();
+        }
+
+        public synchronized void terminate() {
+            isTerminate = true;
+            notifyAll();
+        }
+    }
+
+    public static class MatricesPair {
+        public float[][] matrix1;
+        public float[][] getMatrix2;
+    }
+}
