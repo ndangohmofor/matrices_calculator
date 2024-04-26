beea7ae7d3ade5f03ca0f7ea2aac8b9a8813d9d5
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-25 23:44:47 -0400

    add readmatrix method to parse a matrix from file

--- src/main/java/org/example/MainApplication.java
+++ src/main/java/org/example/MainApplication.java
@@ -6,6 +6,8 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class MainApplication {
+    private static final int N = 10;
+
     public static void main(String[] args) {
     }
 
@@ -13,9 +15,24 @@ public class MainApplication {
         private Scanner scanner;
         private ThreadSafeQueue queue;
 
-        public MatricesReaderProducer(FileReader reader, ThreadSafeQueue queue){
+        public MatricesReaderProducer(FileReader reader, ThreadSafeQueue queue) {
             this.scanner = new Scanner(reader);
             this.queue = queue;
+        }
+
+        private float[][] readMatrix() {
+            float[][] matrix = new float[N][N];
+            for (int r = 0; r < N; r++) {
+                if (!scanner.hasNext()) {
+                    return null;
+                }
+                String[] line = scanner.nextLine().split(",");
+                for (int c = 0; c < N; c++) {
+                    matrix[r][c] = Float.valueOf(line[c]);
+                }
+            }
+            scanner.nextLine();
+            return matrix;
         }
     }
 
